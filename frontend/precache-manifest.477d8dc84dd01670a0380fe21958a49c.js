ad58f7da2414d8cb4f7ea5bf7ac6e782bbb2824a
SAV2 <whissper.winder@gmail.com>
2019-10-11 18:58:46 +0300

    migrated to ReactJS frontend

---
+++ frontend/precache-manifest.477d8dc84dd01670a0380fe21958a49c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27615897a16212f3e11df9576947c56",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b1b96057c2fe69bb13bb",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "23ae6cdfbef06efafa44",
+    "url": "./static/css/main.98b19577.chunk.css"
+  },
+  {
+    "revision": "b1b96057c2fe69bb13bb",
+    "url": "./static/js/2.e6058330.chunk.js"
+  },
+  {
+    "revision": "23ae6cdfbef06efafa44",
+    "url": "./static/js/main.0604c6d2.chunk.js"
+  },
+  {
+    "revision": "73d3dcbf6e1825c14a07",
+    "url": "./static/js/runtime-main.6e5fcb77.js"
+  }
+]);
